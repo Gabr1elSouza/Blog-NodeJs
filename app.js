c7599e08e2cdbdf2dfcc9b06a7e2abea981ae132
Gabriel Souza <gablgdario03@gmail.com>
2023-06-29 00:29:01 -0300

    Add routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require("body-parser")
 const app = express()
+const admin = require('./routes/admin')
 //Configurações
     //Body Parser
     app.use(bodyParser.urlencoded({extended: false}))
@@ -10,7 +11,17 @@ const app = express()
     //Handeblars
     app.engine('handlebars',handlebars.engine({defaultLayout:'main'}))
     app.set('view engine','handlebars')
+    //Sequilize
 //Rotas
+    app.get('/', (req,res) =>{
+        res.send("Rota principal")
+    })
+    app.get('/posts',(req,res) =>{
+        res.send('Lista Posts')
+    })
+
+    app.use('/admin', admin)
+
 
 //Outros
 const PORT = 8081
