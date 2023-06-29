59815f00dff8c538f92ebee6f5cb9e32c0d025aa
Gabriel Souza <gablgdario03@gmail.com>
2023-06-29 00:25:16 -0300

    Add routes

---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res) =>{
+    res.send("Pagina principal do painel ADM")
+
+})
+
+router.get('/posts',(req,res) =>{
+    res.send("Pagina de posts")
+
+})
+
+router.get('/categorias',(req,res) =>{
+    res.send("Página de catefori")
+
+})
+
+
+module.exports = router
